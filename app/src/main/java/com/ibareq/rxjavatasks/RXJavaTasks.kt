988fa13654185927127c85465b0c6621cfe3979a
Nooralden <nooraldenakel@gmail.com>
2021-09-20 03:56:46 +0300

    make the alphabetList private

--- app/src/main/java/com/ibareq/rxjavatasks/RXJavaTasks.kt
+++ app/src/main/java/com/ibareq/rxjavatasks/RXJavaTasks.kt
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit
  */
 
 object RXJavaTasks {
-    val alphabetList = listOf("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+    private val alphabetList = listOf("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
     /**
      * Complete below function
      * let it emit characters form A to Z each 1 second
